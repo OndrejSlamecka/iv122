6f29808b51dbe3dcb44b9b782cfc58d5431e6d4e
Ondrej Slamecka <ondrej@slamecka.cz>
2015-02-22 20:41:32 +0100

    warmup: gcd steps

--- assets/warmup/colours.py
+++ assets/warmup/colours.py
@@ -12,6 +12,6 @@ def gen_gradient(red_function, image_name):
     img.save(image_name)
 
 gen_gradient(lambda i: 255 - i, 'colours.png')
-gen_gradient(lambda i: round((255 - i) * (1+(i/255)*(i/255))), 'colours_more_red.png')
-gen_gradient(lambda i: round((255 - i) * ((1 + i/255)*(1 + i/255))), 'colours_much_more_red.png')
+gen_gradient(lambda i: round((255 - i) * (1+(i/255)**2)), 'colours_more_red.png')
+gen_gradient(lambda i: round((255 - i) * ((1 + i/255)**2)), 'colours_much_more_red.png')
 
