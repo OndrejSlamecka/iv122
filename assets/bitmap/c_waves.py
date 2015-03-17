e39cf4f39e935ac9367377744718146ad7e6e2c5
Ondrej Slamecka <ondrej@slamecka.cz>
2015-03-17 16:55:37 +0100

    cv04/c -- waves

--- assets/bitmap/c_waves.py
+++ assets/bitmap/c_waves.py
@@ -1,14 +1,21 @@
 from PIL import Image
-from math import sqrt, sin
+from math import sin, ceil, sqrt
 
 side = 400
 
 img = Image.new('RGB', (side,side), 'white')
 pixels = img.load()
 
-for i in range(side):
-    for j in range(side):
-        pass # need for sleep...
+for i in range(-side//2, side//2):
+    for j in range(-side//2, side//2):
+        d = sqrt(i*i + j*j)
+        c = ceil(((sin(1/5*d) + 1) * 128))
+        pixels[side//2 + i, side//2 + j] = (c,c,c)
+
+for i in range(-side//3, side//3):
+    for j in range(-side//3, side//3):
+        r,g,b = pixels[side//2 + i, side//2 + j]
+        pixels[side//2 + i, side//2 + j] = (255 - r, 255 - g, 255 - b)
 
 img = img.resize((side // 2, side // 2), Image.ANTIALIAS)
 img.save('c_waves.png')
