8b798e25fb44c9d7602b804b642fecb35eabcf48
Ondrej Slamecka <ondrej@slamecka.cz>
2015-03-21 08:46:02 +0100

    Convex hull

--- assets/geom_alg/hull.py
+++ assets/geom_alg/hull.py
@@ -1,11 +1,18 @@
-from math import sin, cos, pi
 import random
 import svgwrite
 
-plane_size = 500
+# normal distribution
+norm_npoints = 500
+mu = 200
+sigmas = [10, 20, 50, 100] # std. deviation
+offset = 100 # to see points with negative coords
 
-def render(lines, points):
-    dwg = svgwrite.Drawing('hull.svg', profile='tiny')
+# uniform
+uni_npoints = 100
+uni_range = 500
+
+def render(sigma, lines, points):
+    dwg = svgwrite.Drawing('hull_' + str(sigma) + '.svg', profile='tiny')
     stroke = svgwrite.rgb(10,10,16,'%')
 
     for f, t in lines:
@@ -13,16 +20,21 @@ def render(lines, points):
 
     for p in points:
         dwg.add(dwg.circle(p, r = 3))
-    
+
     dwg.save()
 
-def rand_point():
-    x = random.normalvariate(plane_size / 2, plane_size / 4)
-    y = random.normalvariate(plane_size / 2, plane_size / 4)
+# See https://docs.python.org/3.4/library/random.html
+def norm_rand_point(sigma):
+    x = random.normalvariate(mu, sigma) + offset
+    y = random.normalvariate(mu, sigma) + offset
     return round(x), round(y)
 
-# Graham scan, http://en.wikipedia.org/wiki/Graham_scan
+def uni_rand_point():
+    x = random.randint(0, uni_range)
+    y = random.randint(0, uni_range)
+    return x, y
 
+# Graham scan, http://en.wikipedia.org/wiki/Graham_scan
 def orientation(p1, p2, p3):
     # >0    if p1, p2, p3 are cw
     # <0    if ccw
@@ -30,46 +42,30 @@ def orientation(p1, p2, p3):
     return (p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0])
 
 def hull(points):
-    """
-    i = points.index(min(points, key = lambda p: p[1]))
-    points[1], points[i] = points[i], points[1]
-    points.sort(key = lambda p: p[1])
-
-    points[0] = points[len(points) - 1]
-
-    m = 1
-    for i in range(2, len(points)):
-        while orientation(points[m-1], points[m], points[i]) <= 0:
-            if m > 1:
-                m -= 1
-            elif i == len(points):
-                break
-            else:
-                i += 1
-
-        m += 1
-        points[m], points[i] = points[i], points[m]
-
-    return points[:m+1]
-    """
-    
-    U, L = [],[]
-    points.sort()
+    points.sort(key=lambda p: p[1])
+    top, bottom = [],[]
     for p in points:
-        while len(U) > 1 and orientation(U[-2], U[-1], p) <= 0:
-            U.pop()
-        while len(L) > 1 and orientation(L[-2], L[-1], p) >= 0:
-            L.pop()
-        U.append(p)
-        L.append(p)
-    return U+L
+        while len(top) > 1 and orientation(top[-2], top[-1], p) <= 0:
+            top.pop()
+        while len(bottom) > 1 and orientation(bottom[-2], bottom[-1], p) >= 0:
+            bottom.pop()
+        top.append(p)
+        bottom.append(p)
+    bottom.reverse()
+    return top + bottom
 
-points = [rand_point() for _ in range(40)]
+# Run
+def compute_and_render(sigma, points):
+    h = hull(points)
+    lines = []
+    for i in range(len(h) - 1):
+        lines.append((h[i], h[i+1]))
 
-h = hull(points)
-lines = []
-for i in range(len(h) - 1):
-    lines.append((h[i], h[i+1]))
+    render(sigma, lines, points)
 
-render(lines, points)
+for sigma in sigmas:
+    points = [norm_rand_point(sigma) for _ in range(norm_npoints)]
+    compute_and_render(sigma, points)
 
+points = [uni_rand_point() for _ in range(uni_npoints)]
+compute_and_render('uniform', points)
