cebfd9873dfee1e0c0995862a93bcb206c68f2ae
Ondrej Slamecka <ondrej@slamecka.cz>
2015-05-30 09:56:36 +0200

    Half of probability problems

--- assets/prob_stats/is_random.py
+++ assets/prob_stats/is_random.py
@@ -1,18 +1,101 @@
-from random import randint
+#!/usr/bin/env python3
 
-k = input()
+from random import randint
+import sys
+
+def chisq_s(expected, observed):
+    """
+    One value of the sum -- http://www.stat.yale.edu/Courses/1997-98/101/chigf.htm
+    """
+    return ((observed - expected) ** 2) / expected
+
+
+if len(sys.argv) != 2:
+    print('Pass the number k in the name of the dataset <random[k].txt> ' \
+          'in the first argument', file=sys.stderr)
+    sys.exit(1)
+
+# Read input
+k = sys.argv[1]
 file = open("random" + k + ".txt")
 line = file.readline()
 numbers = list(map(int, line.split()))
 
-freq = [0 for _ in range(7)]
-p = 0
-for n in numbers:
-    freq[n] += 1
+def freq(numbers):
+    # Calculate expected frequency
+    expected = len(numbers) / 6
 
-    r = randint(1,6) 
-    if n != r:
-        p += (n-r)**3
+    # Count frequencies
+    freq = [0 for _ in range(7)]
+    p = 0
+    for n in numbers:
+        freq[n] += 1
 
-print("Numbers: ", len(numbers), ", sum: ", sum(numbers), ", (N*3.5) = ", len(numbers)*3.5, ", penalty: ", p)
-print(freq[1:7])
+    # Transform into set of triplets (die roll, frequency, chisq(expected, frequency))
+    freq = freq[1:7]
+    s = []
+    for i, f in enumerate(freq):
+        s.append((i+1, f, chisq_s(expected, f)))
+
+    # Print
+    for val in s:
+        print('{}\t{}\t{:.4f}'.format(*val))
+
+    print('--------'*3)
+    chisq = sum(map(lambda v: v[2], s))
+    print('\tchi^2:\t{:.4f}'.format(chisq))
+
+def is_palindrom(numbers):
+    s = []
+    for i in range(len(numbers) // 2):
+        s.append(numbers[i])
+
+    for i in range(len(numbers) // 2, len(numbers)):
+        n = s.pop()
+        if numbers[i] != n:
+            return False
+
+    return True
+
+
+def pattern_freq(l, pattern):
+    freq = 0
+    p = l.find(pattern, 0)
+    while True:
+        freq += 1
+        p = l.find(pattern, p + len(pattern))
+        if p == -1:
+            break
+
+    return freq
+
+
+def pattern_finder(line):
+    line = line[:]
+    line = line.replace(' ', '')
+
+    # Coefficient for slight benevolence in pattern repetition
+    coef = 1.05
+
+    max_pat = ''
+    max_pat_freq = 0
+
+    for s in range(0,100):  # String starting at 0th to 100th character
+        for l in range(6,150):  # Of length from 6 to 100
+            pat = line[s:s+l]
+            pat_freq = pattern_freq(line, pat)
+
+            # Pattern frequency should match its length
+            if (pat_freq > max_pat_freq and pat_freq > len(line) / (len(pat) * coef)) \
+                or (pat_freq == max_pat_freq and len(pat) > len(max_pat)):
+                max_pat_freq = pat_freq
+                max_pat = pat
+
+    return (max_pat, max_pat_freq)
+
+freq(numbers)
+print('Is a palindrom:', is_palindrom(numbers))
+
+pat_r = pattern_finder(line)
+print('Pattern repetition: {}, length = {}, frequency = {}'.format(pat_r[0],
+                                                    len(pat_r[0]), pat_r[1]))
--- assets/prob_stats/mh.py
+++ assets/prob_stats/mh.py
@@ -1,32 +1,97 @@
+#!/usr/bin/env python3
+
 from random import randint
 
-N = 100000
+"""
+ An experimental evaluation of the Monty Hall problem, that is
+ a game between a player and Monty
+
+ https://xkcd.com/1282/
+"""
+
+N = 1000*1000  # Repeat the experiment N times
+
+def problem_instance(): return [0,1,2]
+
+def unveil_door(treasure, guess):
+    """
+     The MC unveils the single door which
+       a) was not picked by the player,
+       b) do not contain the treasure
+    """
+    s = problem_instance()
+    s.remove(guess)
+    if treasure in s:
+        s.remove(treasure)
+        return s[0]
+    else:
+        return s[randint(0,1)]  # randint range is inclusive
+
+def smart_player(guess, door_shown_by_monty):
+    """
+     The strategy of the smart player is to always change his choice
+     to the door which was a) not shown by Monty, b) not the first choice
+    """
+    s = problem_instance()
+    s.remove(guess)
+    s.remove(door_shown_by_monty)
+    return s[0]
+
+def random_player(door_shown_by_monty):
+    """
+     The strategy of the smart player is to always change his choice
+     to the door which was a) not shown by Monty, b) not the first choice
+    """
+    s = problem_instance()
+    s.remove(door_shown_by_monty)
+    return s[randint(0,1)]
 
 def mh():
+    """
+    Emulate one round of the Monty Hall game.
+    """
+
+    # Create the doors and place the treasure
+    s = problem_instance()
     treasure = randint(0,2)
+
+    # The player picks a door
     guess = randint(0,2)
 
-    # smart
-    if treasure == guess:
-        smart = 0
-    else:
-        smart = 1 
+    # The MC unveils one door
+    door_to_show_to_player = unveil_door(treasure, guess)
 
-    # stay
-    if treasure == guess:
-        stay = 1
-    else:
-        stay = 0
+    ## We proceed to emulate three different strategies of the player
+    success = [0,0,0]  # Vector of success of each strategy
 
-    return (smart, stay)
+    # Smart player
+    door_to_pick = smart_player(guess, door_to_show_to_player)
 
+    if door_to_pick == treasure:
+        success[0] += 1
 
-smart_success = 0
-stay_success = 0
-for i in range(N):
-    r_smart, r_stay = mh()
-    smart_success += r_smart
-    stay_success += r_stay
+    # Not so smart player
+    door_to_pick = guess
 
-print("Smart: ", smart_success, "/", N)
-print("Stay: ", stay_success, "/", N)
+    if door_to_pick == treasure:
+        success[1] += 1
+
+    # Mr. Random
+    door_to_pick = random_player(door_to_show_to_player)
+
+    if door_to_pick == treasure:
+        success[2] += 1
+
+    return success
+
+###
+
+if __name__ == "__main__":
+    success = [0,0,0]  # Vector of success of each strategy
+    for i in range(N):
+        success = list(map(sum, zip(success, mh())))
+
+    print('Smart:', success[0], '/', N, '=', success[0] / N * 100, '%')
+    print('Stay:', success[1], '/', N, '=', success[1] / N * 100, '%')
+    print('Random:', success[2], '/', N, '=', success[2] / N * 100, '%')
+
